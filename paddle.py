3acef8e8c84e28556201ddecc30b3fe0708468b9
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-16 19:17:07 -0800

    add paddle constants

--- paddle.py
+++ paddle.py
@@ -1,27 +1,30 @@
 from turtle import Turtle
 
 MOVE_DISTANCE = 20
+MAX_Y = 260
+PADDLE_COLOR = "white"
+PADDLE_SHAPE = "square"
 
 
 class Paddle(Turtle):
     def __init__(self, position):
         super().__init__()
-        self.shape("square")
-        self.color("white")
+        self.shape(PADDLE_SHAPE)
+        self.color(PADDLE_COLOR)
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
 
     def up(self):
         new_y = self.ycor() + MOVE_DISTANCE
-        if new_y == 260 or new_y == -260:
+        if new_y == MAX_Y or new_y == -MAX_Y:
             pass
         else:
             self.goto(self.xcor(), new_y)
 
     def down(self):
         new_y = self.ycor() - MOVE_DISTANCE
-        if new_y == 260 or new_y == -260:
+        if new_y == MAX_Y or new_y == -MAX_Y:
             pass
         else:
             self.goto(self.xcor(), new_y)
