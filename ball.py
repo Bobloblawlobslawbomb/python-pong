82c3692be522a0b8c2ea3379a678fbae87637302
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-16 19:16:50 -0800

    add ball constants

--- ball.py
+++ ball.py
@@ -1,17 +1,21 @@
 from turtle import Turtle
 
 MOVE_DISTANCE = 10
+BALL_SHAPE = "square"
+BALL_COLOR = "white"
+MOVE_SPEED = 0.1
+MOVE_SPEED_MULTIPLER = 0.9
 
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("square")
-        self.color("white")
+        self.shape(BALL_SHAPE)
+        self.color(BALL_COLOR)
         self.penup()
         self.x_move = MOVE_DISTANCE
         self.y_move = MOVE_DISTANCE
-        self.move_speed = 0.1
+        self.move_speed = MOVE_SPEED
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -23,10 +27,10 @@ class Ball(Turtle):
 
     def bounce_paddle(self):
         self.x_move *= -1
-        self.move_speed *= 0.9
+        self.move_speed *= MOVE_SPEED_MULTIPLER
 
     def reset_position(self):
         self.goto(0, 0)
-        self.move_speed = 0.1
+        self.move_speed = MOVE_SPEED
         self.bounce_paddle()
         self.bounce_wall()
